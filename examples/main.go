12adb03a0b117e81c4ed8324a6f263e9d2a54b4a
sujit <s.baniya.np@gmail.com>
2024-05-13 11:01:38 +0545

    Initial Commit

--- examples/main.go
+++ examples/main.go
@@ -3,24 +3,26 @@ package main
 import (
 	"encoding/json"
 	"os"
-
+	
 	v2 "github.com/oarkflow/ftp-server"
+	"github.com/oarkflow/ftp-server/fs/afos"
 	"github.com/oarkflow/ftp-server/fs/s3"
 	"github.com/oarkflow/ftp-server/models"
+	"github.com/oarkflow/ftp-server/utils"
 )
 
 type config struct {
-	Users       map[string]models.User `json:"users"`
-	BindAddress string                 `json:"bind"`
-	Filepath    string                 `json:"files"`
-	Port        int                    `json:"port"`
-	ReadOnly    bool                   `json:"readOnly"`
+	Users    map[string]models.User `json:"users"`
+	Address  string                 `json:"address"`
+	Filepath string                 `json:"files"`
+	Port     int                    `json:"port"`
+	ReadOnly bool                   `json:"readOnly"`
+	S3       s3.Option              `json:"s3"`
 }
 
 func main() {
 	// Read the config.json.
 	var conf config
-	var s3Opt s3.Option
 	configFile, err := os.ReadFile("config.json")
 	if err != nil {
 		panic(err)
@@ -29,16 +31,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	s3Config, err := os.ReadFile("creds.json")
-	if err != nil {
-		panic(err)
-	}
-	err = json.Unmarshal(s3Config, &s3Opt)
-	if err != nil {
-		panic(err)
-	}
-	// filesystem := afos.New(utils.AbsPath(""))
-	filesystem, err := s3.New(s3Opt)
+	filesystem := afos.New(utils.AbsPath(conf.Filepath))
+	// filesystem, err := s3.New(s3Opt)
 	if err != nil {
 		panic(err)
 	}
